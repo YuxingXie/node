6017a4ec38f438cca01e315be0a62b5bdc79fd6b
Xieyuxing <185246042@qq.com>
2017-04-17 17:46:15 +0800

    a

--- helloworld.js
+++
@@ -1,7 +0,0 @@
-var http=require("http");
-http.createServer(function(req,res){
-    res.writeHeader(200,{'Content-type':'text/plain'})
-    res.end('hello world');
-}).listen(3000);
-
-console.log("server start on localhost:3000;press Ctrl-C to terminate...")
